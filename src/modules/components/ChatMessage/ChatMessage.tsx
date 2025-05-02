92214c42cd8cf7228d7dd505c44e97e835da50ce
IShatrov <shatrov2004@mail.ru>
2025-05-02 11:31:45 +0300

    Better chat

---
+++ src/modules/components/ChatMessage/ChatMessage.tsx
@@ -0,0 +1,69 @@
+import { Avatar, Box, Group, Text, Paper, Stack, useMantineTheme, Title } from '@mantine/core';
+
+import defaultProfilePicture from '../../../assets/default_profile_picture.png';
+
+interface MessageProps {
+  avatar: string;
+  nickname: string;
+  message: string;
+  time: string;
+  date: string;
+  isCurrentUser: boolean;
+}
+
+export function ChatMessage({ avatar, nickname, message, time, date, isCurrentUser }: MessageProps) {
+  const theme = useMantineTheme();
+  
+  return (
+    <Box
+      style={{
+        display: 'flex',
+        justifyContent: isCurrentUser ? 'flex-end' : 'flex-start',
+        marginBottom: 'md',
+      }}
+    >
+      <Paper
+        withBorder={true}
+        p="sm"
+        radius="md"
+        shadow="sm"
+        style={{
+          maxWidth: '90%'
+        }}
+      >
+        <Stack>
+          <Group>
+            {!isCurrentUser && (
+              <Avatar 
+                src={avatar} 
+                size="sm" 
+                radius="xl" 
+                alt={defaultProfilePicture}
+              />
+            )}
+            
+            <Title
+                order={5}
+            >
+              {nickname}
+            </Title>
+            
+            <Text size="xs" >
+                {time} • {date}
+            </Text>
+          </Group>
+          
+          <Text 
+            size="sm"
+            style={{
+              whiteSpace: 'pre-wrap',
+              wordBreak: 'break-word',
+            }}
+          >
+            {message}
+          </Text>
+        </Stack>
+      </Paper>
+    </Box>
+  );
+}
